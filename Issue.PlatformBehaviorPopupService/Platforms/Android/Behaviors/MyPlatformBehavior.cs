8a0d50fbb5340addfed9748e3c958ab1515414e2
bengavin <Ben.Gavin@corebts.com>
2024-06-04 06:40:40 -0500

    UPDATE: Add android behavior

---
+++ Issue.PlatformBehaviorPopupService/Platforms/Android/Behaviors/MyPlatformBehavior.cs
@@ -0,0 +1,18 @@
+﻿namespace Issue.PlatformBehaviorPopupService.Behaviors;
+
+public partial class MyPlatformBehavior : PlatformBehavior<VisualElement>
+{
+    protected override void OnAttachedTo(VisualElement bindable, Android.Views.View platformView)
+    {
+        base.OnAttachedTo(bindable, platformView);
+        System.Diagnostics.Debug.WriteLine($"Attached to: {bindable.Id}");
+    }
+
+    protected override void OnDetachedFrom(VisualElement bindable, Android.Views.View platformView)
+    {
+        base.OnDetachedFrom(bindable, platformView);
+        System.Diagnostics.Debug.WriteLine($"Detached from: {bindable.Id}");
+    }
+}
+
+
